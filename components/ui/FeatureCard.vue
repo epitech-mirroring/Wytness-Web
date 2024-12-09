3de905e5206d470d5174886a1d5cd8114dba98d0
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-12-09 13:58:26 +0100

    🎨 Use of variables for the svg balise of the icons in the project description section
    Add the svg tag directly as a parameter to the FeaturedCard and add a variable containing the svg.

--- components/ui/FeatureCard.vue
+++ components/ui/FeatureCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-6 rounded-xl shadow-lg bg-white flex flex-col items-center text-center">
-    	<div class="text-4xl mb-4">{{ icon }}</div>
+		<div class="text-4xl mb-4" v-html="icon"></div>
     	<h3 class="text-xl font-semibold text-gray-800">{{ title }}</h3>
     	<p class="text-gray-600 mt-2">{{ description }}</p>
     </div>

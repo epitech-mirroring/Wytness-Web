278465e89db455708f75d9476ca82c61f266837f
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-12-10 13:42:11 +0100

    ✨ Add Design page
    Add the front part of the design page. WIP link to the back-end

---
+++ components/ui/ActionCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+      :class="[
+        'p-4 rounded-lg shadow-md cursor-pointer',
+        selected ? 'bg-blue-100 border-blue-600' : 'bg-gray-50 hover:bg-gray-100',
+      ]"
+      @click="$emit('select')"
+    >
+      <div class="text-3xl mb-2">{{ icon }}</div>
+      <h3 class="text-lg font-semibold">{{ title }}</h3>
+      <p class="text-gray-600 text-sm">{{ description }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps(['title', 'description', 'icon', 'selected']);
+  </script>
+  

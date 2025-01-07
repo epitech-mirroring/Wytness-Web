fd7dc1ead2016034d9b907958d0978297ab940fc
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2025-01-07 14:54:07 +0100

    ✨ New landing page
    Deletion of the old landing page and addition of the new landing page.

---
+++ components/WindowCard.vue
@@ -0,0 +1,170 @@
+<template>
+    <div :class="$style.frameParent">
+        <div :class="$style.frameRedButton" />
+        <div :class="$style.frameYellowButton" />
+        <div :class="$style.frameGreenButton" />
+
+        <template v-if="variant === 'card1'">
+            <div :class="$style.frameChild" id="frameChild1Format1"/>
+            <div :class="$style.frameChild" id="frameChild2Format1" />
+            <div :class="$style.frameChild" id="frameChild3Format1" />
+            <div :class="$style.frameChild" id="frameChild4Format1" />
+        </template>
+
+        <template v-else-if="variant === 'card2'">
+            <div :class="$style.frameChildFormat2" id="frameChild1Format2" />
+            <div :class="$style.frameChildFormat2" id="frameChild2Format2" />
+            <div :class="$style.frameChildFormat2" id="frameChild3Format2" />
+            <div :class="$style.frameChildFormat2" id="frameChild4Format2" />
+            <div :class="$style.frameChildFormat2" id="frameChild5Format2" />
+            <div :class="$style.frameChildFormat2" id="frameChild6Format2" />
+            <div :class="$style.frameChildFormat2" id="frameChild7Format2" />
+        </template>
+
+
+    </div>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'WindowCard',
+        props: {
+            variant: {
+            type: String,
+            required: true,
+            validator: (value) => ['card1', 'card2'].includes(value),
+            },
+        },
+    });
+</script>
+<style  module>
+    .frameRedButton {
+        position: absolute;
+        top: 15px;
+        left: 16px;
+        border-radius: 32px;
+        background-color: #ea695f;
+        width: 8px;
+        height: 8px;
+        overflow: hidden;
+    }
+    .frameYellowButton {
+        position: absolute;
+        top: 15px;
+        left: 29px;
+        border-radius: 32px;
+        background-color: #f2be4e;
+        width: 8px;
+        height: 8px;
+        overflow: hidden;
+    }
+    .frameGreenButton {
+        position: absolute;
+        top: 15px;
+        left: 42px;
+        border-radius: 32px;
+        background-color: #66c455;
+        width: 8px;
+        height: 8px;
+        overflow: hidden;
+    }
+    .frameChild, .frameChildFormat2, .frameDiv {
+        position: absolute;
+        border-radius: 19px;
+        overflow: hidden;
+    }
+    .frameChildFormat2 {
+        transform:  rotate(90deg);
+        transform-origin: 0 0;
+    }
+    .frameParent {
+        width: 254px;
+        position: relative;
+        box-shadow: 0px 3px 14.4px -2px rgba(0, 0, 0, 0.25);
+        backdrop-filter: blur(39.5px);
+        border-radius: 21px;
+        background-color: rgba(255, 255, 255, 0.75);
+        height: 191px;
+        overflow: hidden;
+    }
+</style>
+<style>
+    #frameChild1Format1 {
+        top: 52px;
+        left: 16px;
+        background-color: #d9d9d9;
+        width: 123px;
+        height: 12px;
+    }
+    #frameChild2Format1 {
+        top: 75px;
+        left: 16px;
+        background-color: #d9d9d9;
+        width: 146px;
+        height: 12px;
+    }
+    #frameChild3Format1 {
+        top: 98px;
+        left: 16px;
+        background-color: #d9d9d9;
+        width: 139px;
+        height: 12px;
+    }
+    #frameChild4Format1 {
+        top: 150px;
+        left: 16px;
+        background-color: #000;
+        width: 91px;
+        height: 12px;
+    }
+    #frameChild1Format2 {
+        top: 92px;
+        left: 62px;
+        background-color: #000;
+        width: 82px;
+        height: 12px;
+    }
+    #frameChild2Format2 {
+        top: 66px;
+        left: 91px;
+        background-color: #776de3;
+        width: 108px;
+        height: 12px;
+    }
+    #frameChild3Format2 {
+        top: 108px;
+        left: 120px;
+        background-color: #000;
+        width: 66px;
+        height: 12px;
+    }
+    #frameChild4Format2 {
+        top: 40px;
+        left: 149px;
+        background-color: #776de3;
+        width: 134px;
+        height: 12px;
+    }
+    #frameChild5Format2 {
+        top: 54px;
+        left: 178px;
+        background-color: #000;
+        width: 120px;
+        height: 12px;
+    }
+    #frameChild6Format2 {
+        top: 134px;
+        left: 207px;
+        background-color: #ea695f;
+        width: 40px;
+        height: 12px;
+    }
+    #frameChild7Format2 {
+        top: 126px;
+        left: 238px;
+        background-color: #000;
+        width: 48px;
+        height: 12px;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import FeatureCard from "~/components/ui/FeatureCard.vue";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "vue-router";
+import WindowCard1 from "~/components/WindowCard.vue";
 
 const router = useRouter();
 
@@ -34,109 +35,310 @@ const services = [
 ];
 </script>
 
+
 <template>
-  <div
-    class="min-h-screen bg-gray-50 flex flex-col items-center justify-center"
-  >
-    <header class="w-full p-6 mt-20 text-center">
-      <h1 class="text-5xl font-bold text-gray-900">
-        Automate. Connect. Simplify.
-      </h1>
-      <p class="text-xl text-gray-600 mt-4">
-        Revolutionize the way you integrate and automate your workflows.
+  <LandingNavBar />
+
+  <div class="flex ms-[6vw] mt-20">
+    <div class="mt-20 ms-12 sm:ms-24 md:mx-0 xl:mx-0">
+      <p id="title">
+        <span id="automate-faster">Automate faster, </span>
+        <span id="work-less">work less</span>
       </p>
-    </header>
+      <p id="subtitle">
+        Wytness connects all your services
+        so you can spend more time doing
+        what you are good at. ⚡️
+      </p>
+    </div>
 
-    <main class="flex flex-col items-center w-full">
-      <div class="mt-8">
-        <Button
-          class="text-4xl"
-          size="extra_lg"
-          @click="router.push('auth/login')"
-        >
-          Get Started
-        </Button>
-      </div>
-      <section
-        class="mt-20 mb-8 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl"
-      >
-        <FeatureCard
-          title="Connect"
-          description="Easily link your favorite apps and tools."
-          :icon="connectIcon"
-        />
-        <FeatureCard
-          title="Automate"
-          description="Eliminate repetitive tasks with powerful workflows."
-          :icon="automateIcon"
-        />
-        <FeatureCard
-          title="Simplify"
-          description="Streamline your processes and save time."
-          :icon="simplifyIcon"
-        />
-      </section>
-      <section class="mt-10 pt-12 pb-10 w-full bg-white">
-        <div class="max-w-6xl mx-auto">
-          <h2 class="text-4xl font-bold text-gray-800 text-center mb-8">
-            Supported Services
-          </h2>
-          <div class="grid grid-cols-3 md:grid-cols-6 gap-4">
-            <div
-              v-for="service in services"
-              :key="service.name"
-              class="flex flex-col items-center justify-center p-4"
-            >
-              <img
-                :src="service.logo"
-                :alt="service.name"
-                class="h-16 w-16 object-contain"
-              />
-              <p class="mt-3 text-md font-medium text-gray-700">
-                {{ service.name }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+    <!-- Workflow Example -->
+    <div class="h-0 hidden md:block ms-[-150px] min-[820px]:ms-[-100px] min-[870px]:ms-[-50px] min-[930px]:ms-[0px] min-[1010px]:ms-[50px] xl:ms-[-10px] min-[1350px]:ms-[-20px] 2xl:ms-[5vw]">
+      <img id="workflow-button-1" src="/assets/images/landing_page/workflow-button-1.png" alt="Workflow Example" />
+      <img id="workflow-button-2" src="/assets/images/landing_page/workflow-button-2.png" alt="Workflow Example" />
+      <img id="start-arrow" src="/assets/images/landing_page/start-arrow.png" alt="Workflow Example" />
+      <img id="end-arrow" src="/assets/images/landing_page/end-arrow.png" alt="Workflow Example" />
+      <img id="cursor" src="/assets/images/landing_page/cursor.png" alt="Workflow Example" />
+      <div id="full-line-bottom"></div>
+      <div id="full-line-top"></div>
+      <svg id="dashed-line" width="212" height="14" viewBox="0 0 212 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_66_56)">
+          <path d="M205 6C206.657 6 208 4.65685 208 3C208 1.34315 206.657 0 205 0V6ZM7 0C5.34315 0 4 1.34315 4 3C4 4.65685 5.34315 6 7 6V0ZM12.7115 6C14.3684 6 15.7115 4.65685 15.7115 3C15.7115 1.34315 14.3684 0 12.7115 0V6ZM26.0385 0C24.3816 0 23.0385 1.34315 23.0385 3C23.0385 4.65685 24.3816 6 26.0385 6V0ZM37.4615 6C39.1184 6 40.4615 4.65685 40.4615 3C40.4615 1.34315 39.1184 0 37.4615 0V6ZM50.7885 0C49.1316 0 47.7885 1.34315 47.7885 3C47.7885 4.65685 49.1316 6 50.7885 6V0ZM62.2115 6C63.8684 6 65.2115 4.65685 65.2115 3C65.2115 1.34315 63.8684 0 62.2115 0V6ZM75.5385 0C73.8816 0 72.5385 1.34315 72.5385 3C72.5385 4.65685 73.8816 6 75.5385 6V0ZM86.9615 6C88.6184 6 89.9615 4.65685 89.9615 3C89.9615 1.34315 88.6184 0 86.9615 0V6ZM100.288 0C98.6316 0 97.2885 1.34315 97.2885 3C97.2885 4.65685 98.6316 6 100.288 6V0ZM111.712 6C113.368 6 114.712 4.65685 114.712 3C114.712 1.34315 113.368 0 111.712 0V6ZM125.038 0C123.382 0 122.038 1.34315 122.038 3C122.038 4.65685 123.382 6 125.038 6V0ZM136.462 6C138.118 6 139.462 4.65685 139.462 3C139.462 1.34315 138.118 0 136.462 0V6ZM149.788 0C148.132 0 146.788 1.34315 146.788 3C146.788 4.65685 148.132 6 149.788 6V0ZM161.212 6C162.868 6 164.212 4.65685 164.212 3C164.212 1.34315 162.868 0 161.212 0V6ZM174.538 0C172.882 0 171.538 1.34315 171.538 3C171.538 4.65685 172.882 6 174.538 6V0ZM185.962 6C187.618 6 188.962 4.65685 188.962 3C188.962 1.34315 187.618 0 185.962 0V6ZM199.288 0C197.632 0 196.288 1.34315 196.288 3C196.288 4.65685 197.632 6 199.288 6V0ZM7 6H12.7115V0H7V6ZM26.0385 6H37.4615V0H26.0385V6ZM50.7885 6H62.2115V0H50.7885V6ZM75.5385 6H86.9615V0H75.5385V6ZM100.288 6H111.712V0H100.288V6ZM125.038 6H136.462V0H125.038V6ZM149.788 6H161.212V0H149.788V6ZM174.538 6H185.962V0H174.538V6ZM199.288 6H205V0H199.288V6Z" fill="black"/>
+        </g>
+      </svg>
+    </div>
 
-      <section class="pt-10 w-full bg-blue-50 py-10">
-        <div
-          class="max-w-7xl mx-auto flex flex-col md:flex-row items-center md:justify-between px-6 md:px-12"
-        >
-          <div class="text-center md:text-left">
-            <h2 class="text-3xl font-bold text-gray-800">Download the App</h2>
-            <p class="mt-2 text-gray-600">
-              Get the best experience on your mobile device. Available on iOS
-              and Android.
-            </p>
-          </div>
-          <div class="ms-6 mt-6 md:mt-0 flex justify-center gap-4">
-            <a
-              href="https://www.apple.com/app-store/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/assets/images/badges/appstore.svg"
-                alt="Download on the AppStore"
-                class="w-60"
-              />
-            </a>
-            <a
-              href="https://play.google.com/store"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/assets/images/badges/playstore.png"
-                alt="Download on the PlayStore"
-                class="w-60"
-              />
-            </a>
-          </div>
-        </div>
-      </section>
-    </main>
+    <!-- 14412 -->
+    <div class="h-0 hidden xl:block xl:ms-[-400px] min-[1350px]:ms-[-350px] min-[1450px]:ms-[-300px] 2xl:ms-[-300px] min-[1600px]:ms-[-250px] min-[1650px]:ms-[-200px] min-[1700px]:ms-[-150px] min-[1780px]:ms-[-50px] min-[1880px]:ms-0">
+      <WindowCard id="card1" variant="card1"/>
+      <WindowCard id="card2" variant="card2"/>
+      <img id="blue-gradient-block" src="/assets/images/landing_page/blue-gradient-block.png" alt="Forme 'T' en bleu gradient"/>
+      <img id="white-button" src="/assets/images/landing_page/white-button.png" alt="Boutton blanc de fenêtre d'ordinateur"/>
+      <img id="white-button-2" src="/assets/images/landing_page/blue-button-white-border.png" alt="Boutton avec bordure blanche de fenêtre d'ordinateur"/>
+
+      <!-- Eye -->
+      <svg id="eye" width="282" height="242" viewBox="0 0 282 242" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_68_166)">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M91.9657 155.508L47.0624 97.5769L111.739 68.2232C124.442 61.2303 139.707 58.8096 154.897 62.5552C154.994 62.5792 155.091 62.6035 155.188 62.6279C155.285 62.6512 155.381 62.6747 155.477 62.6985C169.873 66.2484 181.753 74.7336 189.762 85.9087L234.666 143.84L169.989 173.194C157.286 180.186 142.021 182.607 126.832 178.861C126.735 178.838 126.638 178.813 126.541 178.789C126.444 178.766 126.348 178.742 126.251 178.718C111.855 175.168 99.9747 166.683 91.9657 155.508Z" fill="white"/>
+        </g>
+        <g clip-path="url(#clip0_68_166)">
+          <rect x="116.542" y="80.7929" width="66" height="66" rx="33" transform="rotate(13.8527 116.542 80.7929)" fill="black"/>
+          <path d="M140.65 121.9L141.259 85.3587L164.901 94.5593L140.65 121.9Z" fill="#776DE3"/>
+        </g>
+        <defs>
+          <filter id="filter0_d_68_166" x="0.662437" y="18.4013" width="280.403" height="212.614" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+          <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+          <feOffset dy="4"/>
+          <feGaussianBlur stdDeviation="23.2"/>
+          <feComposite in2="hardAlpha" operator="out"/>
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_68_166"/>
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_68_166" result="shape"/>
+          </filter>
+          <clipPath id="clip0_68_166">
+          <rect x="116.542" y="80.7929" width="66" height="66" rx="33" transform="rotate(13.8527 116.542 80.7929)" fill="white"/>
+          </clipPath>
+        </defs>
+      </svg>
+
+      <!-- Visual Area -->
+      <svg id="visual-area" width="231" height="329" viewBox="0 0 231 329" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_bd_68_170)">
+        <path d="M15.848 309.435L19.2125 12.1642C19.2125 12.1642 83.95 19.9794 122.986 35.2314C162.021 50.4834 214.909 88.627 214.909 88.627L15.848 309.435Z" fill="white" fill-opacity="0.75" shape-rendering="crispEdges"/>
+        </g>
+        <defs>
+          <clipPath id="visual-area-clip">
+            <path d="M15.848 309.435L19.2125 12.1642C19.2125 12.1642 83.95 19.9794 122.986 35.2314C162.021 50.4834 214.909 88.627 214.909 88.627L15.848 309.435Z" fill="white" fill-opacity="0.75" shape-rendering="crispEdges"/>
+          </clipPath>
+          <filter id="filter0_bd_68_170" x="0.548022" y="0.864245" width="229.66" height="327.871" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feGaussianBlur in="BackgroundImageFix" stdDeviation="4.5"/>
+            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_68_170"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dy="4"/>
+            <feGaussianBlur stdDeviation="7.65"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="effect1_backgroundBlur_68_170" result="effect2_dropShadow_68_170"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow_68_170" result="shape"/>
+        </filter>
+        </defs>
+      </svg>
+
+      <img id="panel" src="/assets/images/landing_page/panel.png" alt="Panel"/>
+    </div>
+
+    <div id="logos">
+      <img id="discord_logo" src="/assets/images/landing_page/discord_logo.png" alt="Logo Discord"/>
+      <img id="spotify_logo" src="/assets/images/landing_page/spotify_logo.png" alt="Logo Spotify"/>
+      <img id="slack_logo" src="/assets/images/landing_page/slack_logo.png" alt="Logo Slack"/>
+    </div>
+
   </div>
 </template>
+
+<style>
+  #title {
+    width: 478px;
+    height: 259px;
+
+    font-family: 'Parkinsans';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 96px;
+    line-height: 100%;
+    letter-spacing: -0.03em;
+
+    color: #000000;
+  }
+
+  #automate-faster::after {
+    content: '';
+    display: inline-block;
+    width: 84px;
+    height: 84px;
+    background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 84 84'%3E%3Cpath d='M42 10.5C42 27.897 56.103 42 73.5 42C56.103 42 42 56.103 42 73.5C42 56.103 27.897 42 10.5 42C27.897 42 42 27.897 42 10.5Z' fill='%23F4D06F'/%3E%3C/svg%3E");
+    background-size: contain;
+    background-repeat: no-repeat;
+    position: relative;
+    top: 15px;
+    left: -1px;
+  }
+
+  #work-less::after {
+    content: '';
+    display: inline-block;
+    width: 435px;
+    height: 13px;
+    background: #F4D06F;
+    position: relative;
+    top: -95px;
+    z-index: -1;
+  }
+
+  #subtitle {
+    width: 361px;
+
+    font-family: 'Parkinsans';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 125%;
+
+    color: #5C5C5C;
+
+    margin-top: 5rem;
+  }
+
+  #card1 {
+    left: 240px;
+    top: -38px;
+  }
+
+  #card2 {
+    left: 650px;
+    top: -100px;
+    z-index: 10;
+  }
+
+  #blue-gradient-block {
+    position: relative;
+    width: 390px;
+    height: 676px;
+    left: 450px;
+    top: -400px;
+    z-index: -10;
+  }
+
+  #white-button {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    left: 770px;
+    top: -1060px;
+  }
+
+  #white-button-2 {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    left: 790px;
+    top: -1080px;
+  }
+
+  #eye {
+    position: relative;
+    width: 273px;
+    height: 213;
+    left: 430px;
+    top: -780px;
+    z-index: 15;
+  }
+
+  #visual-area {
+    position: relative;
+    left: 550px;
+    top: -1210px;
+    clip-path: url(#visual-area-clip);
+    backdrop-filter: blur(4.2px);
+  }
+
+  #panel {
+    position: relative;
+    left: 619px;
+    top: -1100px;
+  }
+
+  #workflow-button-1 {
+    position: relative;
+    width: 80px;
+    height: 80px;
+    left: 0px;
+    top: 500px;
+  }
+
+  #start-arrow {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    left: 78px;
+    top: 372px;
+  }
+
+  #full-line-bottom {
+    position: relative;
+    width: 72px;
+    height: 100px;
+    left: 90px;
+    top: 200px;
+    flex-shrink: 0;
+
+    border-radius: 0px 0px 40px 0px;
+    border-right: 6px solid #000;
+    border-bottom: 6px solid #000;
+
+    z-index: -1;
+  }
+
+  #full-line-top {
+    position: relative;
+    width: 72px;
+    height: 100px;
+    left: 156px;
+    top: 0px;
+    flex-shrink: 0;
+
+    border-radius: 40px 0px 0px 0px;
+    border-left: 6px solid #000;
+    border-top: 6px solid #000;
+  }
+
+  #end-arrow {
+    position: relative;
+    width: 35px;
+    height: 35px;
+    left: 212px;
+    top: 149px;
+
+    z-index: 1;
+  }
+
+  #workflow-button-2 {
+    position: relative;
+    width: 80px;
+    height: 80px;
+    left: 240px;
+    top: 225px;
+  }
+
+  #dashed-line {
+    position: relative;
+    left: 140px;
+    top: 94px;
+
+    z-index: -1;
+  }
+
+  #cursor {
+    position: relative;
+    width: 28px;
+    height: 28px;
+    left: 342px;
+    top: 323px;
+  }
+
+  #logos {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 400px;
+    height: 10px;
+
+    position: absolute;
+    left: 100px;
+    top: 900px;
+  }
+
+</style>
